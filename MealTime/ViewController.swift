10e4314c13ae68beba61fd9257a011768dc1ce9d
aksilont <aksilont@gmail.com>
2021-05-29 09:53:50 +0300

    Initial Commit

---
+++ MealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTime
+//
+//  Created by Aksilont on 29.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
